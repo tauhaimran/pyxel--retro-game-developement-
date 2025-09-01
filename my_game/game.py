f1d875eb5abd018f41a62a2627336c0f5d33497f
Tauha Imran <161915837+tauhaimran@users.noreply.github.com>
2025-09-01 09:20:55 +0500

    Basic Movement added
    Player Moves
    Stone Falls
    Collision Mechanism is active

--- my_game/game.py
+++ my_game/game.py
@@ -13,7 +13,9 @@ class App:
         pyxel.load("my_resource.pyxres")
         self.player_x = SCREEN_WIDTH // 2
         self.player_y = SCREEN_HEIGHT * 4 // 5
-        self.number = 0
+        self.stone_y = 0 
+        self.stone_x = SCREEN_WIDTH // 2
+        self.collision = False
         #self.x = 0
         
         pyxel.run(self.update, self.draw)
@@ -23,15 +25,33 @@ class App:
         if pyxel.btnp(pyxel.KEY_ESCAPE): #if escape key is pressed
             pyxel.quit() #exit the game
 
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            self.player_x += 2 #move right
-        elif pyxel.btn(pyxel.KEY_LEFT):   
-            self.player_x -= 2 #move left
+        if pyxel.btn(pyxel.KEY_RIGHT) and self.player_x < SCREEN_WIDTH - 13:
+            self.player_x += 1 #move right
+        elif pyxel.btn(pyxel.KEY_LEFT) and self.player_x > -3 :   
+            self.player_x -= 1 #move left
+        
+        self.stone_y += 1 #rock falls down
+        if self.stone_y > SCREEN_HEIGHT: #if rock goes out of screen
+            self.stone_y = 0 #reset rock position to top
+
+        #collision detection
+        if (self.stone_y + 8 >= self.player_y)        \
+            and (self.stone_y <= self.player_y + 16)  \
+            and (self.stone_x + 8 >= self.player_x)   \
+            and (self.stone_x <= self.player_x + 16):
+            self.collision = True
+            #print("Collision detected!")
+        else:
+            self.collision = False
         
     def draw(self):
         pyxel.cls(pyxel.COLOR_DARK_BLUE) #clear the screen with dark blue color
-        pyxel.blt( SCREEN_WIDTH/2 - 10, 0, 0, 8, 0, 8, 8, pyxel.COLOR_BLACK) # ROCK
-        pyxel.blt( SCREEN_WIDTH/2 , SCREEN_HEIGHT*4 // 5, 0, 16, 0, 16,16, pyxel.COLOR_BLACK) #player
+        pyxel.blt( SCREEN_WIDTH/2 - 10, self.stone_y, 0, 8, 0, 8, 8, pyxel.COLOR_BLACK) # ROCK
+        pyxel.blt( self.player_x , SCREEN_HEIGHT*4 // 5, 0, 16, 0, 16,16, pyxel.COLOR_BLACK) #player
+
+        if self.collision:
+            pyxel.text(SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2, "GAME OVER", pyxel.COLOR_YELLOW)
+
         pass
 
 App() #run the game
